1cf2bdadee606d9e456d5c9d8b5768f6a235cae3
David <avatar_4500@mail.ru>
2021-02-19 00:03:47 +0300

    finished pairs creation api

--- image_utils.py
+++ image_utils.py
@@ -5,81 +5,111 @@ import numpy as np
 import time
 
 
-def get_dataset_info(main_folder_name,
-                     calculate=False,
-                     estimate=False,
-                     max_positive=None,
-                     max_negative=None,
-                     calculated_positive=0,
-                     calculated_negative=0):
+def get_dataset_info(main_folder_name):
     #
-    # calculate memory size to load all pairs 
+    # report summury of dataset 
+    #
+    
+    total_number=0
+    folders=os.listdir(main_folder_name)
+    positive_pairs_count=0
+    negative_pairs_count=0
+    
+    image_weight_folder=folders[0]
+    folder_image_name=os.listdir(main_folder_name+'/'+image_weight_folder)[0]
+    image=imread(main_folder_name+'/'+ image_weight_folder+'/'+folder_image_name)
+    image_weight=image.nbytes
+    image_shape=image.shape
+
+    # number of classes
+    for folder in folders:
+        total_number+=len(os.listdir(main_folder_name+'/'+folder))
+
+    # counting total amount of pairs
+    for folder in folders:
+        l=len(os.listdir(main_folder_name+'/'+folder))
+        positive_pairs_count+=l**2
+        negative_pairs_count+=l*(total_number-l)
+
+    # calculating images weight
+    positive_pairs_weight=round((image_weight*positive_pairs_count*2)/(1024**3),3)
+    negative_pairs_weight=round((image_weight*negative_pairs_count*2)/(1024**3),3)
+
+
+    number_classes=len(os.listdir(main_folder_name))
+    
+    print('-------------------------------------------------------')
+    print('Dataset info\n')
+    print('image shape = ',image_shape)
+    print('image weight = ',round(image_weight/1024,3),' KiB')
+    print('number of classes = ',number_classes)
+    print('number of pictures = ',total_number)
+
+
+    print('positive pairs count =',positive_pairs_count)
+    print('positive pairs images weight =',positive_pairs_weight,' GiB')
+
+    print('negative pairs count =',negative_pairs_count)
+    print('negative pairs images weight =',negative_pairs_weight,' GiB')
+
+    
+def calculate_dataset(main_folder_name,
+                     max_positive,
+                     max_negative,
+                     calculated_positive,
+                     calculated_negative ):
+    #
+    #  calculate memory size to load all pairs
     #
 
-    image_weight=50 # KiB
-    if not calculate and not estimate:
-        
-        total_number=0
-        folders=os.listdir(main_folder_name)
-        positive_pairs_count=0
-        negative_pairs_count=0
+    folder=os.listdir(main_folder_name)[0]
+    folder_image_name=os.listdir(main_folder_name+'/'+folder)[0]
+    image=imread(main_folder_name+'/'+folder+'/'+folder_image_name)
+    image_weight=image.nbytes
 
-        # number of classes
-        for folder in folders:
-            total_number+=len(os.listdir(main_folder_name+'/'+folder))
+    header='Calculated info \n'
+    calculated_positive_pairs_weight=round((image_weight*calculated_positive*2)/(1024**3),3)
+    calculated_negative_pairs_weight=round((image_weight*calculated_negative*2)/(1024**3),3)
 
-        # counting total amount of pairs
-        for folder in folders:
-            l=len(os.listdir(main_folder_name+'/'+folder))
-            positive_pairs_count+=l**2
-            negative_pairs_count+=l*(total_number-l)
-            
-        # calculating images weight
-        positive_pairs_weight=(image_weight*positive_pairs_count*2)/(1024**2)   
-        negative_pairs_weight=(image_weight*negative_pairs_count*2)/(1024**2)  
+    print('-------------------------------------------------------')
+    print(header)
 
+    print('desired positive pairs count =',max_positive)
+    print('calculated positive pairs count =',calculated_positive)
+    print('positive pairs images weight =',calculated_positive_pairs_weight,' GiB')
+    print('\ndesired negative pairs count =',max_negative)
+    print('calculated negative pairs count =',calculated_negative)
+    print('negative pairs images weight =',calculated_negative_pairs_weight,' GiB')
 
-        number_classes=len(os.listdir(main_folder_name))
-        print('-------------------------------------------------------')
-        print('Dataset info\n')
-        print('number of classes = ',number_classes)
-        print('number of pictures = ',total_number)
-
-
-        print('positive pairs count =',positive_pairs_count)
-        print('positive pairs images weight GiB =',positive_pairs_weight)
-
-        print('negative pairs count =',negative_pairs_count)
-        print('negative pairs images weight GiB =',negative_pairs_weight)
-
-    else:
-        # if need to estimate image 
-        header='Calculated info \n'
-        calculated_positive_pairs_weight=(image_weight*calculated_positive*2)/(1024**2)   
-        calculated_negative_pairs_weight=(image_weight*calculated_negative*2)/(1024**2)
-        
-        if estimate:
-            header='Estimated info\n'
-            calculated_positive_pairs_weight=(image_weight*max_positive*2)/(1024**2)   
-            calculated_negative_pairs_weight=(image_weight*max_negative*2)/(1024**2)
-            
     
-        
-        
-        print('-------------------------------------------------------')
-        print(header)
+def estimate_dataset(main_folder_name,
+                     max_positive,
+                     max_negative ):
+    #
+    # estimate memory size to load all pairs 
+    #
 
-        print('desired positive pairs count =',max_positive)
-        print('calculated positive pairs count =',calculated_positive)
-        print('positive pairs images weight GiB =',calculated_positive_pairs_weight)
-        print('\ndesired negative pairs count =',max_negative)
-        print('calculated negative pairs count =',calculated_negative)
-        print('negative pairs images weight GiB =',calculated_negative_pairs_weight)
+    folder=os.listdir(main_folder_name)[0]
+    folder_image_name=os.listdir(main_folder_name+'/'+folder)[0]
+    image=imread(main_folder_name+'/'+folder+'/'+folder_image_name)
+    image_weight=image.nbytes
+
+    header='Estimated info\n'
+    calculated_positive_pairs_weight=round((image_weight*max_positive*2)/(1024**3),3)
+    calculated_negative_pairs_weight=round((image_weight*max_negative*2)/(1024**3),3)
+
+    print('-------------------------------------------------------')
+    print(header)
+
+    print('desired positive pairs count =',max_positive)
+    print('positive pairs images weight =',calculated_positive_pairs_weight,' GiB')
     
+    print('\ndesired negative pairs count =',max_negative)
+    print('negative pairs images weight =',calculated_negative_pairs_weight,' GiB')
 
     
     
-def get_pairs(main_folder_name,image_shape,max_positive_pairs_count,max_negative_pairs_count):
+def get_pairs(main_folder_name,max_positive_pairs_count,max_negative_pairs_count):
     #
     # creating positive pairs sum Li**2 pairs 
     #
@@ -87,10 +117,17 @@ def get_pairs(main_folder_name,image_shape,max_positive_pairs_count,max_negative
     
     labels = []
     pair_len=2
-    pairs_num=max_positive_pairs_count+max_negative_pairs_count
-    shape = [pairs_num,pair_len]+list(image_shape)
-    pairs = np.zeros(shape,dtype='uint8')
     folders=os.listdir(main_folder_name)
+    pairs_num=max_positive_pairs_count+max_negative_pairs_count
+    
+    folder=os.listdir(main_folder_name)[0]
+    folder_image_name=os.listdir(main_folder_name+'/'+folder)[0]
+    image=imread(main_folder_name+'/'+folder+'/'+folder_image_name)
+    
+    image_shape=image.shape
+    pairs_shape = [pairs_num,pair_len]+list(image_shape)
+    pairs = np.zeros(pairs_shape,dtype='uint8')
+    
     positive_counter=0
     
     while positive_counter <max_positive_pairs_count:
@@ -146,13 +183,11 @@ def get_pairs(main_folder_name,image_shape,max_positive_pairs_count,max_negative
         labels.append([0])
         negative_counter+=1
     
-    get_dataset_info(main_folder_name,
-                     True,
-                     False,
-                     max_positive_pairs_count,
-                     max_negative_pairs_count,
-                     positive_counter,
-                     negative_counter-positive_counter)
+    calculate_dataset(main_folder_name,
+                      max_positive_pairs_count,
+                      max_negative_pairs_count,
+                      positive_counter,
+                      negative_counter-positive_counter)
     print('\npassed seconds: ',time.clock()-start_time)
     return pairs,labels
     
